a461ee341945225fcdef0cb0d4ea31a239044f44
kxkl2077 <q15711383123@outlook.com>
2026-09-19 20:12:25 +0800

    chore: release 0.0.12

--- mc-plugin/src/main/java/cn/stalir/mcbridge/Version.java
+++ mc-plugin/src/main/java/cn/stalir/mcbridge/Version.java
@@ -11,7 +11,7 @@ package cn.stalir.mcbridge;
  */
 public final class Version {
 
-    public static final String VERSION = "0.0.11";
+    public static final String VERSION = "0.0.12";
 
     private Version() {
     }
